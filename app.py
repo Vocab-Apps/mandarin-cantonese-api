b4ba0aaec9c3bbaa7efebc6de6aa589fb8ab76b4
Luc Wastiaux <luc.wastiaux@xsmail.com>
2019-09-10 22:04:52 +0800

    conversiontype only do error checking

--- app.py
+++ app.py
@@ -40,11 +40,7 @@ class Batch(Resource):
 
         print(f"conversion_type: {conversion_type} entries: {entry_list}")
 
-        if conversion_type == 'pinyin':
-            conversion_function = pinyin_jyutping_sentence.pinyin
-        elif conversion_type == 'jyutping':
-            conversion_function = pinyin_jyutping_sentence.jyutping
-        else:
+        if conversion_type != 'pinyin' and conversion_type != 'jyutping':
             return {'status': 'error', 'description': 'incorrect conversion argument: ' + conversion_type}
 
         result_list = [perform_conversion(x, conversion_type, tone_numbers) for x in entry_list]
