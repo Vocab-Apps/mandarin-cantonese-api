377926ad792ca9b50f849bb1be6c39d9613114b3
Luc W <lucw@airpost.net>
2024-03-24 09:19:25 +0800

    bump up version

--- google_sheets_function.js
+++ google_sheets_function.js
@@ -256,7 +256,7 @@ function call_api(input_array, format, tone_numbers, spaces) {
       'spaces': spaces,
       'entries': query_array,
       'user_uuid': get_user_uuid(),
-      'addon_version': 'v32'
+      'addon_version': 'v33'
     };
     //console.log(data);
     var options = {
